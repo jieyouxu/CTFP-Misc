54f4d590c653d45dd5e358851802a1537669c507
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-23 17:37:47 +0800

    Add composition implementation

---
+++ Exercises/E-1-4/compose.js
@@ -0,0 +1,13 @@
+// Composition between two functions:
+// Given functions
+//  f :: a -> b
+//  g :: b -> c
+// Then their composition is
+//  (g . f) :: a -> c
+
+// compose :: (a -> b) -> (b -> c) -> (a -> c)
+const compose = f => g => {
+    // Note a new function is returned with a single parameter `x` which is
+    // the composition between `f` and `g` with `f` applied first.
+    return x => g(f(x));
+};
