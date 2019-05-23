70ce200088ecd4eb29f94611aa9eb283e03cdcc4
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2019-05-23 18:17:18 +0800

    Change to uncurried version

--- Exercises/E-1-4/compose.js
+++ Exercises/E-1-4/compose.js
@@ -6,10 +6,10 @@
 //  (g . f) :: a -> c
 
 // compose :: (a -> b) -> (b -> c) -> (a -> c)
-const compose = f => g => {
-  // Note a new function is returned with a single parameter `x` which is
-  // the composition between `f` and `g` with `f` applied first.
-  return x => g(f(x));
+const compose = (f, g) => {
+    // Note a new function is returned with a single parameter `x` which is
+    // the composition between `f` and `g` with `f` applied first.
+    return x => g(f(x));
 };
 
 export { compose };
